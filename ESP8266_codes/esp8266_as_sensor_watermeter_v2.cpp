da0e37b28780136869c9a0c1e02dda18f55f31a4
mike <muhammedjepbarov@gmail.com>
2020-11-02 00:02:30 +0500

    v2 sketch of watermeter

--- ESP8266_codes/esp8266_as_sensor_watermeter_v2.cpp
+++ ESP8266_codes/esp8266_as_sensor_watermeter_v2.cpp
@@ -3,31 +3,6 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 
-// water sensor setup
-byte sensorInterrupt = 0;  // 0 = digital pin 2
-byte sensorPin1      = 2;
-byte sensorInterrupt2 = 0;
-byte sensorPin2      = 4;
-
-// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
-// litre/minute of flow.
-float calibrationFactor = 4.5;
-
-volatile byte pulseCount;  
-volatile byte pulseCount2;  
-
-float flowRate;
-float flowRate2;
-
-unsigned int flowMilliLitres;
-unsigned int flowMilliLitres2;
-unsigned long totalMilliLitres;
-unsigned long totalMilliLitres2;
-
-unsigned long oldTime;
-unsigned long pTime = 30000;
-unsigned long oldTime2 = 0;
-//////
 
 //Static IP address configuration
 IPAddress staticIP(192, 168, 1, 223); //ESP static ip
@@ -46,27 +21,34 @@ String command = "water_measurer_sensor";
 ESP8266WebServer server(80);
 
 
+// water sensor setup
+volatile int flow_frequency; // Measures flow sensor pulses
+unsigned int l_hour; // Calculated litres/hour
+unsigned char flowsensor = 2; // Sensor Input
+unsigned long currentTime;
+unsigned long cloopTime;
+
+void flow () // Interrupt function
+{
+   flow_frequency++;
+}
+//////
+
+
 void handlePong() {
   server.send(200, "text/html", device_key);
 }
 
 
 void setup(){  
-  pinMode(sensorPin1, INPUT);
-  digitalWrite(sensorPin1, 1);
-  pinMode(sensorPin2, INPUT);
-  digitalWrite(sensorPin2, 1);
-
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-
-  pulseCount2        = 0;
-  flowRate2          = 0.0;
-  flowMilliLitres2   = 0;
-  totalMilliLitres2  = 0;
-  oldTime           = 0;
+  // **** measurer setup **** //
+  pinMode(flowsensor, INPUT);
+  digitalWrite(flowsensor, HIGH); // Optional Internal Pull-Up
+  attachInterrupt(0, flow, RISING); // Setup Interrupt
+  sei(); // Enable interrupts
+  currentTime = millis();
+  cloopTime = currentTime;
+  // *********************** //
 
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -103,50 +85,19 @@ void loop(){
 
 
 void measureAndSend() {
-  if(millis() - oldTime2 >= 5000)
+  currentTime = millis();
+  if(currentTime >= (cloopTime + 5000))
   {
-    int value = totalMilliLitres/1000*2;
-    int value2 = totalMilliLitres2/1000*2;
-    unsigned int total_value = value + value2;
+    cloopTime = currentTime; // Updates cloopTime
+    // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
+    l_hour = (flow_frequency * 60 / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
+    flow_frequency = 0; // Reset Counter
+    // Serial.print(l_hour, DEC); // Print litres/hour
+    // Serial.println(" L/hour");
 
-    String argument_data = "?device_key="+device_key+"&command="+command+"&value="+String(total_value);
+    String argument_data = "?device_key="+device_key+"&command="+command+"&value="+String(l_hour);
     sendRequest("http://"+serverUrl+"/esp/ArgToDB/",argument_data);
-
-    oldTime2 = millis();
-    totalMilliLitres = 0;
-    totalMilliLitres2 = 0;
   }
-  if((millis() - oldTime) > 1000)    // Only process counters once per second
-  {
-    detachInterrupt(sensorInterrupt);
-    detachInterrupt(sensorInterrupt2);
-
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    flowRate2 = ((1000.0 / (millis() - oldTime)) * pulseCount2) / calibrationFactor;
-
-    oldTime = millis();
-
-    flowMilliLitres = (flowRate / 60) * 1000;
-    flowMilliLitres2 = (flowRate2 / 60) * 1000;
-    
-    totalMilliLitres += flowMilliLitres;
-    totalMilliLitres2 += flowMilliLitres2;
-      
-    unsigned int frac;
-    
-    pulseCount = 0;
-    pulseCount2 = 0;
-    
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-    attachInterrupt(sensorInterrupt2, pulseCounter, FALLING);
-  }
-}
-
-
-void pulseCounter()
-{
-  pulseCount++;
-  pulseCount2++;
 }
 
 
@@ -161,6 +112,7 @@ void sendRequest(String path, String sendingData){
     Serial.println("WiFi Disconnected");
   }
 }
+
 
 String httpGETRequest(const char* serverName) {
   HTTPClient http;
