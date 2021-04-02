53813fb604041c5134b173856e8e01b921fb8040
Mike Bionic <muhammedjepbarov@gmail.com>
2021-04-02 12:10:52 +0500

    issue on slave resolved

--- electronics_arduino_codes/Master_Slave/masterSketch/masterSketch.ino
+++ electronics_arduino_codes/Master_Slave/masterSketch/masterSketch.ino
@@ -1,46 +1,40 @@
 #include <SoftwareSerial.h>
 SoftwareSerial master_controller(2,3);
-SoftwareSerial slave_controller(4,5);
-
-int led = 13;
+SoftwareSerial slave_controller1(4,5);
+SoftwareSerial slave_controller2(6,7);
+SoftwareSerial slave_controller3(8,9);
+SoftwareSerial slave_controller4(10,11);
+SoftwareSerial slave_controller5(14,15);
 
 void setup() {
   Serial.begin(9600);
   master_controller.begin(9600);
-  slave_controller.begin(9600);
-  pinMode(led,OUTPUT);
+  slave_controller1.begin(9600);
+  slave_controller2.begin(9600);
+  slave_controller3.begin(9600);
+  slave_controller4.begin(9600);
+  slave_controller5.begin(9600);
 }
 
 void loop() {
-  slave_controller.listen();
-  if(Serial.available() != 0){
+  if(Serial.available()!=0){
     String payload = Serial.readStringUntil('\n');
     payload.trim();
-    if (payload.length()>0){
+		if (payload.length()>0){
 
-      if (payload == "heyhey"){
-        digitalWrite(led, 1);
-      }
-
-      char* cString = (char*) malloc(sizeof(char)*(payload.length() + 1));
-      payload.toCharArray(cString, payload.length() + 1);
-
-      slave_controller.write(cString);
-    }
+			char* cString = (char*) malloc(sizeof(char)*(payload.length() + 1));
+			payload.toCharArray(cString, payload.length() + 1);
+			
+			slave_controller1.write(cString);
+			slave_controller2.write(cString);
+			slave_controller3.write(cString);
+			slave_controller4.write(cString);
+      slave_controller5.write(cString);
+		}
   }
 
-  if (slave_controller.available()){
-    String data = slave_controller.readStringUntil('\n');
-    data.trim();
-    if (data.length() > 0){
-      Serial.println(data);
-      if (data == "off_called"){
-        digitalWrite(led, 0);
-      }
-      if (data == "on_called"){
-        digitalWrite(led, 1);
-      }
-    }
+  if(slave_controller1.available()!=0){
+    Serial.println(slave_controller1.readStringUntil('\n'));
   }
 
 }
--- electronics_arduino_codes/Master_Slave/slaveSketch/slaveSketch_v24.ino
+++ electronics_arduino_codes/Master_Slave/slaveSketch/slaveSketch_v24.ino
@@ -1,46 +1,38 @@
 #include <SoftwareSerial.h>
 SoftwareSerial master_controller(2,3);
 SoftwareSerial slave_controller(4,5);
-
 int led = 13;
-
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
   master_controller.begin(9600);
   slave_controller.begin(9600);
   pinMode(led,OUTPUT);
 }
-
-void loop() {
-  slave_controller.listen();
-  if(Serial.available() != 0){
-    String payload = Serial.readStringUntil('\n');
-    payload.trim();
-    if (payload.length()>0){
-
-      if (payload == "heyhey"){
-        digitalWrite(led, 1);
-      }
-
-      char* cString = (char*) malloc(sizeof(char)*(payload.length() + 1));
-      payload.toCharArray(cString, payload.length() + 1);
-
+void send_uart_message(String payload, String receiver = "master"){
+    char* cString = (char*) malloc(sizeof(char)*(payload.length() + 1));
+  payload.toCharArray(cString, payload.length() + 1);
+  if(receiver == "slave"){
       slave_controller.write(cString);
-    }
   }
-
-  if (slave_controller.available()){
-    String data = slave_controller.readStringUntil('\n');
-    data.trim();
-    if (data.length() > 0){
-      Serial.println(data);
-      if (data == "off_called"){
-        digitalWrite(led, 0);
-      }
-      if (data == "on_called"){
+  else if(receiver == "master"){
+      master_controller.write(cString);
+  }
+}
+void loop() {
+  master_controller.listen();
+ if(master_controller.available()){
+     String payload = master_controller.readStringUntil('\n');
+   payload.trim();
+   Serial.println(payload);
+   if (payload.length()>0){
+      if (payload == "on_1"){
         digitalWrite(led, 1);
-      }
+      send_uart_message("on_called", "master");
     }
-  }
-
+    if (payload == "off_1"){
+        digitalWrite(led, 0);
+      send_uart_message("off_called", "master");
+    }
+   }
+ }
 }
