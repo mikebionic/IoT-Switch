7f46bf20dca8322ac950e84a3b8d284b29ed30e6
mike-bionic <muhammedjepbarov@gmail.com>
2021-05-28 15:42:45 +0500

    added master sketch of arduino

---
+++ electronics_arduino_codes/Master_Slave/masterSketch/masterSketch_v25.ino
@@ -0,0 +1,56 @@
+#include <SoftwareSerial.h>
+
+int led = 13;
+String arduino_process_key = "main_arduino_process_secret_key";
+String command = "";
+String action = "";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() != 0){
+    String stream = Serial.readStringUntil('\n');
+    stream.trim();
+    if (stream.length()>0){
+			command = getStringPartByDelimeter(stream,':',0);
+			action = getStringPartByDelimeter(stream,':',1);
+			process_key = getStringPartByDelimeter(stream,':',2);
+
+			if (process_key == arduino_process_key){
+				control_device(command, action)
+			}
+
+    }
+  }
+}
+
+
+String getStringPartByDelimeter(String data, char separator, int index){
+  int stringData = 0;
+  String dataPart = "";
+  for(int i = 0; i<data.length()-1; i++){
+    if(data[i]==separator) {
+      stringData++;
+    }else if(stringData==index) {
+      dataPart.concat(data[i]);
+    }else if(stringData>index){
+      return dataPart;
+      break;
+    }
+  }
+  return dataPart;
+}
+
+void control_device(String command, String action){
+	if (command == "led13"){
+		if (action == "on"){
+			digitalWrite(led, 1);
+		}
+		else if (action == "off"){
+			digitalWrite(led, 0);
+		}
+	}
+}
--- home_server/main/device_routes/__init__.py
+++ home_server/main/device_routes/__init__.py
@@ -1,2 +1,3 @@
 from .esp_json_to_arg import esp_json_to_arg
 from .esp_arg_to_db import esp_arg_to_db
+from .esp_ping import esp_ping
