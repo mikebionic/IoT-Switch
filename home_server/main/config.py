bf3aeb37401dfdd478039ed6c62047b3cb85ace4
mike <muhammedjepbarov@gmail.com>
2022-01-15 08:45:18 +0500

    updated configs and route

--- home_server/main/config.py
+++ home_server/main/config.py
@@ -8,4 +8,8 @@ class Config:
 	SQLALCHEMY_DATABASE_URI = "sqlite:///SmartSwitches.db"
 	SERVER_URL = "http://127.0.0.1:5000"
 	MASTER_ARDUINO_IP = "127.0.0.1"
+
+	SYSTEM_IP = "192.168.1.252"
+	SYSTEM_MASTER_IP = "192.168.1.254"
+
 	SOUND_PLAYER = "playsound"
